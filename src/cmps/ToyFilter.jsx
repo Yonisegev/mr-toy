72fa6b5dbb001476a821a7557d07e1b63a47a8d2
Reem Ben-David <reemile1996@gmail.com>
2021-12-17 17:48:54 +0200

    changed getlabels

--- src/cmps/ToyFilter.jsx
+++ src/cmps/ToyFilter.jsx
@@ -5,69 +5,75 @@ import { toyService } from '../services/toyService';
 import { onSetFilter } from '../store/actions/toyActions';
 
 export const ToyFilter = () => {
-    const [filterBy, setFilterBy] = useState({
-        word: '',
-        type: '',
-        labels: []
-    })
-    const [options, setOptions] = useState([])
-    const dispatch = useDispatch()
+  const [filterBy, setFilterBy] = useState({
+    word: '',
+    type: '',
+    labels: [],
+  });
+  const [options, setOptions] = useState([]);
+  const dispatch = useDispatch();
 
-    useEffect(() => {
-        loadLabels()
-    }, [loadLabels])
+  useEffect(() => {
+    loadLabels();
+  }, [loadLabels]);
 
-    const loadLabels = useCallback(() => {
-        const labels = toyService.getlabels();
-        const options = labels.map(label => ({ value: label.toLowerCase(), label }))
-        setOptions(options)
-    }, [setOptions])
+  const loadLabels = useCallback(() => {
+    const labels = toyService.getLabels();
+    const options = labels.map((label) => ({
+      value: label.toLowerCase(),
+      label,
+    }));
+    setOptions(options);
+  }, [setOptions]);
 
-    const handleChange = ({target: {name, value}}) => {
-        handleSetFilter(name, value)
-    }
+  const handleChange = ({ target: { name, value } }) => {
+    handleSetFilter(name, value);
+  };
 
-    const handleSelectChange = ({value}) => {
-        if (value.length > 3) return
-        handleSetFilter('labels', value)
-    }
+  const handleSelectChange = ({ value }) => {
+    if (value.length > 3) return;
+    handleSetFilter('labels', value);
+  };
 
-    const handleSetFilter = (filter, value) => {
-        const newFilter = {...filterBy, [filter]: value}
-        setFilterBy(newFilter)
-        dispatch(onSetFilter(newFilter))
-    }
+  const handleSetFilter = (filter, value) => {
+    const newFilter = { ...filterBy, [filter]: value };
+    setFilterBy(newFilter);
+    dispatch(onSetFilter(newFilter));
+  };
 
-    const { word, type, labels } = filterBy
+  const { word, type, labels } = filterBy;
 
-    return (
-        <form className='toy-filter'>
-            <div className='search-img'>
-                <span className='fas fa-search'></span>
-            </div>
+  return (
+    <form className='toy-filter'>
+      <div className='search-img'>
+        <span className='fas fa-search'></span>
+      </div>
 
-            <select name='type' onChange={handleChange} value={type}>
-                <option value=''>All</option>
-                <option value='instock'>In Stock</option>
-                <option value='soldout'>Sold Out</option>
-            </select>
+      <select name='type' onChange={handleChange} value={type}>
+        <option value=''>All</option>
+        <option value='instock'>In Stock</option>
+        <option value='soldout'>Sold Out</option>
+      </select>
 
-            <label htmlFor='by-word' >
-                <input
-                    name='word' id='by-word'
-                    type='text' placeholder='Search by name :' value={word}
-                    onChange={handleChange}
-                />
-            </label>
+      <label htmlFor='by-word'>
+        <input
+          name='word'
+          id='by-word'
+          type='text'
+          placeholder='Search by name :'
+          value={word}
+          onChange={handleChange}
+        />
+      </label>
 
-            <Select
-                className='select'
-                value={labels}
-                isMulti
-                onChange={handleSelectChange}
-                options={options}
-                placeholder='Choose up to 3 labels :'
-            />
-        </form>
-    )
-}
+      <Select
+        className='select'
+        value={labels}
+        isMulti
+        onChange={handleSelectChange}
+        options={options}
+        placeholder='Choose up to 3 labels :'
+      />
+    </form>
+  );
+};
